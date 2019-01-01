f59328f0b0cb9b15748a5585969343c24377d904
pablo flores <klo@MacBook-Air-de-pablo.local>
2019-01-01 12:56:16 -0600

    I made a new Activity and gave funtionality to the text.

--- CA291218/Proyecto_Pablo/app/src/main/java/com/example/klo/myapplication/MainActivity.java
+++ CA291218/Proyecto_Pablo/app/src/main/java/com/example/klo/myapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.klo.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            txt = (TextView) findViewById(R.id.not_account);
+            txt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    linkToRegistry();
+                }
+            });
+
+
     }
+
+    public void linkToRegistry(){
+
+        Intent intent = new Intent(this, RegistryActivity.class);
+        startActivity(intent);
+    }
+
 }
