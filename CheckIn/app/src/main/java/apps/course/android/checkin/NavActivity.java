7065feaa73d3b748f7f4f5365cb12a83f4149ad8
pablo flores <klo@MacBook-Air-de-pablo.local>
2019-01-02 14:59:29 -0600

    I made a Bottom Navigator.

---
+++ CheckIn/app/src/main/java/apps/course/android/checkin/NavActivity.java
@@ -0,0 +1,13 @@
+package apps.course.android.checkin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav);
+    }
+}
