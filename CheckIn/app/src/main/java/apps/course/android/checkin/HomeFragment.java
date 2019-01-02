f2dde7a3ad578b4316a7464a3bbc410a5b85e2da
pablo flores <klo@MacBook-Air-de-pablo.local>
2019-01-02 15:00:46 -0600

    Tried to make a Navigator.

---
+++ CheckIn/app/src/main/java/apps/course/android/checkin/HomeFragment.java
@@ -0,0 +1,19 @@
+package apps.course.android.checkin;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends FragmentActivity {
+
+    @Nullable
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
